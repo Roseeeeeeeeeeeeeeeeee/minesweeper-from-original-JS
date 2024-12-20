8ef171a8292242638990b1b70f72929ff0f957ec
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-12-20 11:59:00 +0800

    工具函数

---
+++ js/tool.js
@@ -0,0 +1,18 @@
+/**
+ * 工具函数
+ */
+
+
+/**
+ * 单一元素选择器
+ */
+function $(target){
+    return document.querySelector(target);
+}
+
+/**
+ * 元素全选器
+ */
+function $$(target){
+    return document.querySelectorAll(target);
+}
